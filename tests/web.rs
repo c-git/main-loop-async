e1d7f062a70061fdce5fc77f4a64da36fe65b713
One <43485962+c-git@users.noreply.github.com>
2026-05-29 09:13:28 -0400

    test: update deadlock test

--- tests/web.rs
+++ tests/web.rs
@@ -4,22 +4,11 @@ use wasm_bindgen_test::wasm_bindgen_test_configure;
 wasm_bindgen_test_configure!(run_in_browser);
 fn main() {
     #[wasm_bindgen_test]
-    async fn test_fetch() -> Result<(), Box<dyn std::error::Error>> {
-        // let client = reqwest::Client::new();
-        // let request = client.get("https://httpbin.org/get");
-        // let (tx, rx) = futures::channel::oneshot::channel();
+    async fn test_for_deadlock() -> Result<(), Box<dyn std::error::Error>> {
+        let rx = main_loop_async::spawn_with_return(async || 200);
 
-        // fetch(
-        //     request,
-        //     move |result: Result<reqwest::Response, reqwest::Error>| async {
-        //         tx.send(result.expect("Expecting Response not Error").status())
-        //             .expect("Receiver should still be available");
-        //     },
-        // );
-
-        // let status = rx.await?; //If we can't block the calling task use try_recv
-        // instead assert_eq!(status, 200);
-        // Ok(())
-        todo!()
+        let task_result = rx.await?; //If we can't block the calling task use try_recv instead
+        assert_eq!(task_result, 200);
+        Ok(())
     }
 }
