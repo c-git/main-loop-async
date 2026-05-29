dce848a183e772f509b58a968340e9671ef1141a
One <43485962+c-git@users.noreply.github.com>
2026-05-29 12:27:23 -0400

    refactor: extract code to show data

--- examples/egui_simple.rs
+++ examples/egui_simple.rs
@@ -8,6 +8,8 @@
 //! cargo run --example egui_simple --features="egui"
 //! ```
 
+use main_loop_async::{DataState, spawn_with_return};
+
 fn main() -> eframe::Result {
     use eframe::egui;
 
@@ -19,7 +21,7 @@ fn main() -> eframe::Result {
     // Our application state:
     let mut name = "Arthur".to_owned();
     let mut age = 42;
-    let mut data_state = main_loop_async::DataState::None;
+    let mut data_state = DataState::None;
     let mut seconds_required_to_load = 10;
 
     eframe::run_ui_native(
@@ -41,37 +43,46 @@ fn main() -> eframe::Result {
 
                 // Data from the spawned task will show here after the user clicks
                 ui.separator();
-                ui.heading("Load Data (NB: Set to randomly fail)");
-                if let Some(data) = data_state.egui_poll_mut(ui, None) {
-                    ui.horizontal(|ui| {
-                        ui.label("Editable Data from spawned task");
-                        ui.text_edit_singleline(data);
-                    });
-                    if ui.button("Clear Data").clicked() {
-                        data_state = main_loop_async::DataState::None;
-                    }
-                } else if data_state.is_none() {
-                    ui.add(
-                        egui::Slider::new(&mut seconds_required_to_load, 0..=30)
-                            .text("Seconds needed to load data"),
-                    );
-                    if ui.button("Spawn task to load data").clicked() {
-                        let can_make_progress =
-                            data_state.egui_start_task(ui, || main_loop_async::spawn_with_return(move || load_data(seconds_required_to_load)));
-                            assert!(
-                                can_make_progress.is_able_to_make_progress(),
-                                "checks that we don't have a logic error, this should always be able to make progress from this point"
-                            );
-                    }
-                } else if data_state.is_awaiting_response() {
-                    // Currently loading allowing the user to abort, might not make sense for your application
-                    if ui.button("Cancel Loading").clicked(){
-                        data_state = main_loop_async::DataState::None;
-                    }
-                }
+                ui_show_data(ui, &mut data_state, &mut seconds_required_to_load);
             });
         },
     )
+}
+
+fn ui_show_data(
+    ui: &mut egui::Ui,
+    data_state: &mut DataState<String>,
+    seconds_required_to_load: &mut u64,
+) {
+    ui.heading("Load Data (NB: Set to randomly fail)");
+    if let Some(data) = data_state.egui_poll_mut(ui, None) {
+        ui.horizontal(|ui| {
+            ui.label("Editable Data from spawned task");
+            ui.text_edit_singleline(data);
+        });
+        if ui.button("Clear Data").clicked() {
+            *data_state = DataState::None;
+        }
+    } else if data_state.is_none() {
+        ui.add(
+            egui::Slider::new(seconds_required_to_load, 0..=30).text("Seconds needed to load data"),
+        );
+        if ui.button("Spawn task to load data").clicked() {
+            let secs = *seconds_required_to_load;
+            let can_make_progress =
+                data_state.egui_start_task(ui, || spawn_with_return(move || load_data(secs)));
+            assert!(
+                can_make_progress.is_able_to_make_progress(),
+                "checks that we don't have a logic error, this should always be able to make progress from this point"
+            );
+        }
+    } else if data_state.is_awaiting_response() {
+        // Currently loading allowing the user to abort, might not make sense for your
+        // application
+        if ui.button("Cancel Loading").clicked() {
+            *data_state = DataState::None;
+        }
+    }
 }
 
 fn create_tokio_runtime() -> tokio::runtime::Runtime {
